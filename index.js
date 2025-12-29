858af9517bb0a339880babbea468dd7338a5c4ea
Exo-bot-dashboard <disconnected7997@gmail.com>
2025-12-29 22:26:02 +0000

    Fix error logging message format

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
 
 // --- Discord client ---
 const client = new Client({
-  client.on("error", function(err) {
-  console.error("CLIENT ERROR:", err);
+  client.on("error", function (err) {
+    console.error("Client error:", err);
 });
 
 client.on("shardError", (err) => {
